e69863712190f01764b164c5c8984dd1de82920f
Jan Halfar <jan@bestbytes.com>
2018-11-08 00:53:25 +0100

    added debug

--- cmd/nodeflock/nodeflock.go
+++ cmd/nodeflock/nodeflock.go
@@ -13,6 +13,6 @@ func main() {
 	flagSize := flag.Int("flock-size", 10, "number of node processes to run")
 	flagMaxExecutionTime := flag.Int("max-execution-time", 10, "max execution time in seconds")
 	flag.Parse()
-	fmt.Println("starting socket server on", *flagAddr, "with a flock size of", *flagSize, "and a max execution time of", *flagMaxExecutionTime, "s")
+	fmt.Println("I AM SURE: starting socket server on", *flagAddr, "with a flock size of", *flagSize, "and a max execution time of", *flagMaxExecutionTime, "s")
 	fmt.Println("exiting with error", nodeflock.RunServer(*flagAddr, *flagSize, flag.Arg(0), time.Duration(*flagMaxExecutionTime)*time.Second))
 }
