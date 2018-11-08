896162d918a25dda7c51296ac51984349bccb45d
Stefan Martinov <stefan.martinov@gmail.com>
2018-11-08 18:34:15 +0100

    feat: add version to nodeflock CMD

--- cmd/nodeflock/nodeflock.go
+++ cmd/nodeflock/nodeflock.go
@@ -8,11 +8,14 @@ import (
 	"github.com/foomo/nodeflock"
 )
 
+var Version string
+
 func main() {
 	flagAddr := flag.String("addr", ":8888", "address to bind to")
 	flagSize := flag.Int("flock-size", 10, "number of node processes to run")
 	flagMaxExecutionTime := flag.Int("max-execution-time", 10, "max execution time in seconds")
 	flag.Parse()
-	fmt.Println("conn debug starting socket server on", *flagAddr, "with a flock size of", *flagSize, "and a max execution time of", *flagMaxExecutionTime, "s")
-	fmt.Println("exiting with error", nodeflock.RunServer(*flagAddr, *flagSize, flag.Arg(0), time.Duration(*flagMaxExecutionTime)*time.Second))
+	fmt.Println("Running version: " + Version)
+	fmt.Println("Starting socket server on", *flagAddr, "with a flock size of", *flagSize, "and a max execution time of", *flagMaxExecutionTime, "s")
+	fmt.Println("Exiting with error", nodeflock.RunServer(*flagAddr, *flagSize, flag.Arg(0), time.Duration(*flagMaxExecutionTime)*time.Second))
 }
