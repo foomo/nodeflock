bc5a44e90199901e92f2afcb04aabcfd0df74978
Stefan Martinov <stefan.martinov@gmail.com>
2018-11-08 19:21:26 +0100

    feat: add metrics listener on port 80

--- cmd/nodeflock/nodeflock.go
+++ cmd/nodeflock/nodeflock.go
@@ -3,21 +3,47 @@ package main
 import (
 	"flag"
 	"fmt"
-	"time"
-
 	"github.com/foomo/nodeflock"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"log"
+	"net/http"
+	"time"
 )
 
 var (
 	version string
 )
 
+const (
+	defaultNodeflockAddress          = ":8888"
+	defaultNodeflockSize             = 10
+	defaultNodeflockMaxExecutionTime = 10
+
+	defaultPrometheusAddress = ":80"
+)
+
 func main() {
-	flagAddr := flag.String("addr", ":8888", "address to bind to")
-	flagSize := flag.Int("flock-size", 10, "number of node processes to run")
-	flagMaxExecutionTime := flag.Int("max-execution-time", 10, "max execution time in seconds")
+	flagAddr := flag.String("addr", defaultNodeflockAddress, "address to bind to")
+	flagSize := flag.Int("flock-size", defaultNodeflockSize, "number of node processes to run")
+	flagMaxExecutionTime := flag.Int("max-execution-time", defaultNodeflockMaxExecutionTime, "max execution time in seconds")
 	flag.Parse()
+
 	fmt.Println("Running version", version)
-	fmt.Println("Starting socket server on", *flagAddr, "with a flock size of", *flagSize, "and a max execution time of", *flagMaxExecutionTime, "s")
-	fmt.Println("Exiting with error", nodeflock.RunServer(*flagAddr, *flagSize, flag.Arg(0), time.Duration(*flagMaxExecutionTime)*time.Second))
+
+	go startMetricsListener()
+	go startNodeflockListener(*flagAddr, *flagSize, *flagMaxExecutionTime)
+
+	select {}
+}
+
+func startMetricsListener() {
+	prometheus := http.NewServeMux()
+	prometheus.HandleFunc("/metrics", promhttp.Handler().ServeHTTP)
+	fmt.Println("Starting metrics listener on ", defaultPrometheusAddress)
+	log.Print(http.ListenAndServe(defaultPrometheusAddress, prometheus))
+}
+
+func startNodeflockListener(address string, size int, maxExecutionTime int) {
+	fmt.Println("Starting socket server on", address, "with a flock size of", size, "and a max execution time of", maxExecutionTime, "s")
+	log.Fatal(nodeflock.RunServer(address, size, flag.Arg(0), time.Duration(maxExecutionTime)*time.Second))
 }
